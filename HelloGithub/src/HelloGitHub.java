fb4bbd21b5055c99356ec2d34ec1f4ecd1b8c804
inwif <inwif@DESKTOP-JMEKAN1>
2018-10-11 23:45:57 +0900

    jh test

--- HelloGithub/src/HelloGitHub.java
+++ HelloGithub/src/HelloGitHub.java
@@ -3,7 +3,7 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Github master Branch");
+System.out.println("Hello Githubf master Branch");
 	}
 
 }
