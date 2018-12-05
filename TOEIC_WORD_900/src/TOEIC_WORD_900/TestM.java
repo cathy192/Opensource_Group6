0c67fadaa9682e5d89eb4800164980c010f46b23
kkmin223 <rkdals133@gmail.com>
2018-12-05 23:56:51 +0900

    test2

--- TOEIC_WORD_900/src/TOEIC_WORD_900/TestM.java
+++ TOEIC_WORD_900/src/TOEIC_WORD_900/TestM.java
@@ -15,65 +15,68 @@ import java.awt.event.ActionEvent;
 
 public class TestM extends JFrame {
 
-	private JPanel TestmPane;
-	static TestM testm_f=new TestM();
-	/**
-	 * Launch the application.
-	 */
-	
+   private JPanel TestmPane;
+   static TestM testm_f=new TestM();
+   /**
+    * Launch the application.
+    */
+   
 
-	/**
-	 * Create the frame.
-	 */
-	public TestM() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 400);
-		TestmPane = new JPanel();
-		TestmPane.setBackground(Color.WHITE);
-		TestmPane.setForeground(Color.BLACK);
-		TestmPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(TestmPane);
-		TestmPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("TOEIC 900");
-		lblNewLabel.setForeground(Color.PINK);
-		lblNewLabel.setFont(new Font("Berlin Sans FB Demi", Font.BOLD, 40));
-		lblNewLabel.setBounds(178, 85, 227, 46);
-		TestmPane.add(lblNewLabel);
-		
-		Button button = new Button("\uC2DC\uD5D81");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				Main.playSound(1.0f, false);
-				testm_f.setVisible(false);
-				word_test_1.test1_f.setVisible(true);
-			}
-		});
-		button.setBackground(Color.WHITE);
-		button.setBounds(232, 162, 87, 25);
-		TestmPane.add(button);
-		
-		Button button_1 = new Button("\uC2DC\uD5D82");
-		button_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				Main.playSound(1.0f, false);
-			}
-		});
-		button_1.setBackground(Color.WHITE);
-		button_1.setBounds(232, 193, 87, 25);
-		TestmPane.add(button_1);
-		
-		Button button_2 = new Button("Main Menu");
-		button_2.setBackground(Color.WHITE);
-		button_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				Main.playSound(1.0f, false);
-				testm_f.setVisible(false);
-				Menu.menu_f.setVisible(true);
-			}
-		});
-		button_2.setBounds(232, 256, 87, 25);
-		TestmPane.add(button_2);
-	}
+   /**
+    * Create the frame.
+    */
+   public TestM() {
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setBounds(100, 100, 600, 400);
+      TestmPane = new JPanel();
+      TestmPane.setBackground(Color.WHITE);
+      TestmPane.setForeground(Color.BLACK);
+      TestmPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+      setContentPane(TestmPane);
+      TestmPane.setLayout(null);
+      
+      JLabel lblNewLabel = new JLabel("TOEIC 900");
+      lblNewLabel.setForeground(Color.PINK);
+      lblNewLabel.setFont(new Font("Berlin Sans FB Demi", Font.BOLD, 40));
+      lblNewLabel.setBounds(178, 85, 227, 46);
+      TestmPane.add(lblNewLabel);
+      
+      Button button = new Button("\uC2DC\uD5D81");
+      button.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent arg0) {
+            Main.playSound(1.0f, false);
+            testm_f.setVisible(false);
+            word_test_1.test1_f.setVisible(true);
+         }
+      });
+      button.setBackground(Color.WHITE);
+      button.setBounds(232, 162, 87, 25);
+      TestmPane.add(button);
+      
+      Button button_1 = new Button("\uC2DC\uD5D82");
+      button_1.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent arg0) {
+            Main.playSound(1.0f, false);
+            testm_f.setVisible(false);
+            word_test_2.test2_f.setVisible(true);
+      
+         }
+      });
+      button_1.setBackground(Color.WHITE);
+      button_1.setBounds(232, 193, 87, 25);
+      TestmPane.add(button_1);
+      
+      Button button_2 = new Button("Main Menu");
+      button_2.setBackground(Color.WHITE);
+      button_2.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent arg0) {
+            Main.playSound(1.0f, false);
+            testm_f.setVisible(false);
+            Menu.menu_f.setVisible(true);
+         }
+      });
+      button_2.setBounds(232, 256, 87, 25);
+      TestmPane.add(button_2);
+   }
 
 }
--- TOEIC_WORD_900/src/TOEIC_WORD_900/word_test_2.java
+++ TOEIC_WORD_900/src/TOEIC_WORD_900/word_test_2.java
@@ -23,129 +23,120 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import javax.swing.JList;
-import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import java.util.Scanner;
 import javax.swing.JLabel;
 
 import java.util.Random;
-public class word_test_1 extends JFrame {
+public class word_test_2 extends JFrame {
 
-   private JPanel test1;
+   private JPanel test2;
    private JTextField wordField;
    private int unit=1;
    private int cnt=0;
    public static File file;
    public static FileReader filereader;
    public static BufferedReader bufReader;
-   private word_unit Study_Unit=wordDAO.getUnit(unit);
-   static word_test_1 test1_f=new word_test_1();
+
+   static word_test_2 test2_f=new word_test_2();
    private JTextField answerField;
+   AdvanceReq User=new AdvanceReq();
+   String wrong=User.getWrong(Main.userInfo.getName(), unit);
+   private word_unit Wrong_Unit=wordDAO.getWrongUnit(unit,wrong);
      
    String answer;
    private int wrongcnt=0;
    private Random rand=new Random();
    private int problem;
-   private JTextField correct;
    private String Wrong="";
    private float percent;
    private boolean pass=false;
    private boolean was=false;
    private boolean press=true;
+   int k=0;
    /**
     * Launch the application.
     */
    
    
       
-   public word_test_1() {
+   public word_test_2() {
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       setBounds(100, 100, 671, 427);
-      test1 = new JPanel();
-      test1.setForeground(Color.BLACK);
-      test1.setBackground(Color.WHITE);
-      test1.setBorder(new EmptyBorder(5, 5, 5, 5));
-      setContentPane(test1);
-      test1.setLayout(null);
+      test2 = new JPanel();
+      test2.setForeground(Color.BLACK);
+      test2.setBackground(Color.WHITE);
+      test2.setBorder(new EmptyBorder(5, 5, 5, 5));
+      setContentPane(test2);
+      test2.setLayout(null);
       Scanner scanner=new Scanner(System.in);
       AdvanceReq advanceReq= new AdvanceReq();
      
-    
+    while(Wrong_Unit.word[k]!=null)
+         k++;
       
       Button button = new Button("\uB2E4\uC74C \uB2E8\uC5B4");
       button.addActionListener(new ActionListener() {
-         public void actionPerformed(ActionEvent arg0) {
-            String line,mean=null,print=null;
-            Main.playSound(1.0f, false);
-            was=false;
-            problem=rand.nextInt(2);
-            if(press) {
-               press=false;
-               answerField.setText("");
-            if(cnt==30) {
-            	percent=(30-wrongcnt)*100/30;
-               if(percent>=70) {
-            	   JOptionPane.showMessageDialog(null, "시험 통과!");
-            	   line="시험 통과";
-                  pass=true;
-                  advanceReq.insertInfo( Main.userInfo.getName(), unit, (int)percent, pass, Wrong);
-               }
-               else {
-            	   JOptionPane.showMessageDialog(null, "다시 하세요.");
-            	   line="다시 하세요.";
-               }
-               
-               
-            }
-            else if(cnt>30) {
-            	line="Unit "+unit+" TEST 종료";
-            }
-            else {
-            	
-               if(problem==0) {
-            	   line=(cnt+1)+".뜻: "+Study_Unit.mean[cnt]+"\n";       
-               }
-               else {
-            	   line=(cnt+1)+".단어: "+Study_Unit.word[cnt]+"\n";
-               }
-                 cnt++;
-                    
-               }
-            wordField.setText(line); 
-               
-                
-            }
-            else {
-               answerField.setText("정답확인 하세요");
-            }
-            
-         }
-      });
+            public void actionPerformed(ActionEvent arg0) {
+                 was=false;
+                 problem=rand.nextInt(2);
+                 if(press) {
+                    press=false;  
+                    answerField.setText("");
+             String line;
+             Main.playSound(1.0f, false);
+             if(cnt>k) {
+                line="Unit "+unit+" TEST 종료";            
+             }
+             else if(Wrong_Unit.word[cnt]==null) {
+                line="오답이 없습니다.";
+             }
+             else {
+             if(problem==0) {
+                   line=(cnt+1)+".뜻: "+Wrong_Unit.mean[cnt]+"\n";       
+                }
+                else {
+                   line=(cnt+1)+".단어: "+Wrong_Unit.word[cnt]+"\n";
+                }
+                  cnt++;
+                     
+                }
+             wordField.setText(line);
+          
+          }
+                 else {
+                     answerField.setText("정답확인 하세요");
+                  }
+                 }
+       });
       button.setBackground(Color.WHITE);
       button.setBounds(398, 290, 122, 25);
-      test1.add(button);
+      test2.add(button);
       
       wordField = new JTextField();
       wordField.setBounds(133, 112, 387, 95);
-      test1.add(wordField);
+      test2.add(wordField);
       wordField.setColumns(10);
       wordField.setText("Unit "+Integer.toString(unit)+" TEST");
       
       JComboBox Unit = new JComboBox();
       Unit.setModel(new DefaultComboBoxModel(new String[] {"Unit 1", "Unit 2", "Unit 3", "Unit 4", "Unit 5", "Unit 6", "Unit 7", "Unit 8", "Unit 9", "Unit 10", "Unit 11", "Unit 12", "Unit 13", "Unit 14", "Unit 15", "Unit 16", "Unit 17", "Unit 18", "Unit 19", "Unit 20", "Unit 21", "Unit 22", "Unit 23", "Unit 24", "Unit 25", "Unit 26", "Unit 27", "Unit 28", "Unit 29", "Unit 30"}));
       Unit.setBounds(133, 61, 94, 24);
-      test1.add(Unit);
+      test2.add(Unit);
       Unit.addItemListener(new ItemListener() {
             public void itemStateChanged(ItemEvent ev) {
                Main.playSound(1.0f, false);
                 answerField.setText("");
-               unit=Unit.getSelectedIndex()+1;
-               Study_Unit=wordDAO.getUnit(unit);
-               wordField.setText("Unit "+Integer.toString(unit)+" TEST");
+                unit=Unit.getSelectedIndex()+1;
+               if(wrong!="") {
+                  Wrong_Unit=wordDAO.getWrongUnit(unit,wrong);
+                  wordField.setText("Unit "+unit);
+               }
+               else {
+                  wordField.setText("Unit "+unit+"에 틀린단어가 없습니다.");
+               }
                 cnt=0;
-                wrongcnt=0;
-                Wrong="";
             }
       });
       Button button_1 = new 
@@ -153,26 +144,26 @@ public class word_test_1 extends JFrame {
       button_1.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent arg0) {
             Main.playSound(1.0f, false);
-            test1_f.setVisible(false);
+            test2_f.setVisible(false);
             TestM.testm_f.setVisible(true);
          }
       });
       button_1.setBackground(Color.WHITE);
       button_1.setBounds(433, 61, 87, 25);
-      test1.add(button_1);
+      test2.add(button_1);
       
       answerField = new JTextField();
       answerField.setBounds(133, 231, 387, 33);
-      test1.add(answerField);
+      test2.add(answerField);
       answerField.setColumns(10);
       
       JLabel label = new JLabel("\uBB38\uC81C");
       label.setBounds(133, 98, 57, 15);
-      test1.add(label);
+      test2.add(label);
       
       JLabel label_1 = new JLabel("\uB2F5");
       label_1.setBounds(133, 217, 57, 15);
-      test1.add(label_1);
+      test2.add(label_1);
       
       Button button_2 = new Button("\uC815\uB2F5\uD655\uC778");
       button_2.addActionListener(new ActionListener() {
@@ -184,8 +175,8 @@ public class word_test_1 extends JFrame {
              }
              else
              {
-            	 if(problem==0) {
-            		 if(Study_Unit.word[cnt-1].equals(answer)) {
+                if(problem==0) {
+                   if(Wrong_Unit.word[cnt-1].equals(answer)) {
                          answerField.setText("맞음");
                          Wrong+="0";
                       }
@@ -193,27 +184,27 @@ public class word_test_1 extends JFrame {
                          answerField.setText("틀림");
                          wrongcnt++;
                          Wrong+="1";
-                         correct.setText(Integer.toString(wrongcnt)+"/30");
+                       //  correct.setText(Integer.toString(wrongcnt)+"/30");
                       
-            	 }
                 }
-            	 else {
-            		 String[] mean_answer=Study_Unit.mean[cnt-1].split("[|]");
-            		 int i;
-            		 for(i=0;i<mean_answer.length;i++) {
-            			 if(mean_answer[i].equals(answer)) {
-            				 answerField.setText("맞음");
+                }
+                else {
+                   String[] mean_answer=Wrong_Unit.mean[cnt-1].split("[|]");
+                   int i;
+                   for(i=0;i<mean_answer.length;i++) {
+                      if(mean_answer[i].equals(answer)) {
+                         answerField.setText("맞음");
                              Wrong+="0";
                              break;
-            			 }
-            		 }
-            		 if(i==mean_answer.length) {
-            			 answerField.setText("틀림 ");
+                      }
+                   }
+                   if(i==mean_answer.length) {
+                      answerField.setText("틀림 ");
                          wrongcnt++;
                          Wrong+="1";
-                         correct.setText(Integer.toString(wrongcnt)+"/30");
-            		 }
-            	 }
+                 //        correct.setText(Integer.toString(wrongcnt)+"/"+k);
+                   }
+                }
              }
              press=true;
              was=true;
@@ -221,16 +212,6 @@ public class word_test_1 extends JFrame {
       });
       button_2.setBackground(Color.WHITE);
       button_2.setBounds(242, 290, 122, 25);
-      test1.add(button_2);
-      
-      correct = new JTextField();
-      correct.setBounds(346, 61, 53, 21);
-      test1.add(correct);
-      correct.setColumns(10);
-      correct.setText("0/30");
-      
-      JLabel label_2 = new JLabel("\uD2C0\uB9B0 \uAC1C\uC218:");
-      label_2.setBounds(277, 64, 57, 15);
-      test1.add(label_2);
+      test2.add(button_2);
    }
 }
